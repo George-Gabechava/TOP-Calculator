c0f488ef9658f10ab65c1ed0d31469c3f03312a2
George-Gabechava <georgegabechava@gmail.com>
2023-08-11 10:06:41 -0400

    Functions are working, need to make UI pretty

---
+++ README.md
@@ -0,0 +1,10 @@
+<!-- 
+Create a calculator with buttons for each digit, and functions for:
+add
+subtract
+multiply
+divide
+and later on: clear
+
+Author: George Gabechava
+-->
---
+++ calculator.js
@@ -0,0 +1,122 @@
+//Operations
+const add = function(a, b) {
+    return a + b;
+}
+
+const subtract = function(a, b) {
+    return a - b;
+}
+
+const muliply = function(a,b) {
+    return a * b;
+}
+
+const divide = function(a,b) {
+    return a / b;
+}
+
+
+const operate = function(strA, op, strB) {
+    let a = Number(strA);
+    let b = Number(strB);
+    afterEqual = true;
+    
+    
+    
+    if (op === '+') {
+        solution = add(a,b);
+        inputA = String(solution);
+        inputB = '';
+        operator = '';
+        toDisplay(inputB || inputA);
+        return solution;
+    }
+
+    if (op === '-') {
+        solution = subtract(a,b);
+        inputA = String(solution);
+        inputB = '';
+        operator = '';
+        toDisplay(inputB || inputA);
+        return solution;
+        return subtract(a,b);
+    }
+
+    if (op === '*') {
+        solution = muliply(a,b);
+        inputA = String(solution);
+        inputB = '';
+        operator = '';
+        toDisplay(inputB || inputA);
+        return solution;
+        return muliply(a,b);
+    }
+
+    if (op === '/') {
+        console.log(strB, typeof(strB));
+        if (strB === '0') {
+            toDisplay('>:( DIVIDED BY 0');
+            return;
+        }
+
+        solution = divide(a,b);
+        inputA = String(solution);
+        inputB = '';
+        operator = '';
+        toDisplay(inputB || inputA);
+        return solution;
+    }
+
+}
+
+//The Math
+let inputA = '0';
+let inputB = '';
+let operator = '';
+let afterEqual = false;
+
+const updateNumber = function(str) {
+
+    if (inputA == '0' || afterEqual == true) {
+        inputA = str;
+        afterEqual = false;
+        operator = '';
+    }
+    else if (operator != '' && afterEqual == false){
+        inputB += str;
+    }
+    
+    else {
+        inputA += str;
+    }
+
+    console.log((str));
+    console.log('a', inputA);
+    console.log('b', inputB);
+    toDisplay(inputB || inputA);
+}
+
+const updateOperator = function(str) {
+    console.log(str);
+    if (inputB) {
+        operate(inputA, operator, inputB);
+        toDisplay(inputA);
+        inputB = '';
+    }
+    operator = str;
+    afterEqual = false;
+}
+
+//Display Screen should update on most presses, but not all. 
+function toDisplay(str1) {
+    document.getElementById("displayScreen").innerText = str1;
+  }
+let displayScreen = toDisplay(inputB || inputA);
+
+//AC button clear slate
+const clearBtn = function() {
+    inputA = '0';
+    inputB = '';
+    operator = '';
+    toDisplay(0);
+}
